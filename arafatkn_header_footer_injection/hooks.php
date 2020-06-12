ee1ec49090bfe912c74f66e35069e559f92dadec
Arafat Islam <arafatkn@gmail.com>
2020-06-12 22:23:45 +0600

    Optimization and Bug Fix
    Database query optimized (only a single query).

--- arafatkn_header_footer_injection/hooks.php
+++ arafatkn_header_footer_injection/hooks.php
@@ -1,36 +1,44 @@
 <?php
-use Illuminate\Database\Capsule\Manager as Capsule;
+use WHMCS\Database\Capsule;
+
+function arafat_hfi_getData()
+{
+    if(isset($_POST["arafat_hfi"]))
+        return $_POST["arafat_hfi"];
+    
+    $query = Capsule::table('tbladdonmodules')->where('module', '=' , 'arafatkn_header_footer_injection');
+
+    $data = [];
+    foreach($query->get() as $res)
+        $data[$res->setting] = $res->value;
+
+    $_POST["arafat_hfi"] = $data;
+
+    return $_POST["arafat_hfi"];
+}
+
+function arafat_hfi_head($vars)
+{   
+    $data = arafat_hfi_getData();
+    
+    return $data["head-enable"] == "on" && $data["head"]? trim($data["head"]): '';
+}
 
 function arafat_hfi_header($vars) 
 {   
-	$isEnabled =  Capsule::table('tbladdonmodules')->select('value')-> WHERE('module', '=' , 'arafatkn_header_footer_injection')->WHERE('setting' , '=', 'header-enable')->WHERE('value' , 'on')->count();
-	if (empty($isEnabled)) {
-		return;
-    }
+    $data = arafat_hfi_getData();
     
-    $code =  Capsule::table('tbladdonmodules')->select('value')-> WHERE('module', '=' , 'arafatkn_header_footer_injection')->WHERE('setting' , '=', 'header')->pluck('value');
-    if (is_array($code)) {
-        $code = current($code);
-    }
-
-    return $code? trim($code): '';
+    return $data["header-enable"] == "on" && $data["header"]? trim($data["header"]): '';
 }
 
 function arafat_hfi_footer($vars) 
-{   
-	$isEnabled =  Capsule::table('tbladdonmodules')->select('value')-> WHERE('module', '=' , 'arafatkn_header_footer_injection')->WHERE('setting' , '=', 'footer-enable')->WHERE('value' , 'on')->count();
-	if (empty($isEnabled)) {
-		return;
-    }
+{
+	$data = arafat_hfi_getData();
     
-    $code =  Capsule::table('tbladdonmodules')->select('value')-> WHERE('module', '=' , 'arafatkn_header_footer_injection')->WHERE('setting' , '=', 'footer')->pluck('value');
-    if (is_array($code)) {
-        $code = current($code);
-    }
-
-    return $code? trim($code): '';
+    return $data["footer-enable"] == "on" && $data["footer"]? trim($data["footer"]): '';
 }
 
-add_hook("ClientAreaFooterOutput",1,"arafat_hfi_footer");
+add_hook("ClientAreaHeadOutput",1,"arafat_hfi_head");
 add_hook("ClientAreaHeaderOutput",1,"arafat_hfi_header");
+add_hook("ClientAreaFooterOutput",1,"arafat_hfi_footer");
 
